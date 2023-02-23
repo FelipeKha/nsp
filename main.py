3e8a88715dfffe076c6df0ed11af4845a6b423ee
FelipeKha <felipe.kharaba@icloud.com>
2023-02-23 14:19:57 +0100

    Add the simulated annealing algorithm, separated our get_neighbour into a new module, and corrected the tabu_search get_neighbour to visit arbitrary number of neighbours instead of the entire solution set.

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import matplotlib.pyplot as plt
 
 from genetic_algo.genetic_algo import GeneticAlgo
+from simulated_annealing.sim_anneal import SimulatedAnnealing
 from tabu_search.tabu_search import TabuSearch
 from utils.covering_cost import CoveringCost
+from utils.get_neighbour import GetNeighbour
 from utils.get_population import GetPopulation
 from validation import Validation
 
@@ -26,6 +28,15 @@ if __name__ == '__main__':
     crossover_prob = 0.7
     mutation_prob = 0.3
 
+    # simulated annealing parameters
+    nb_neighbours = 10
+    # annealing schedule parameters (function that determine the temperature, 
+    # the lower it is the less likely we are to accept a worse solution)
+    # temperature = k * exp(-lam * iter_num) if iter_num < limit, else 0
+    k = 20      # scale up the temperature
+    lam = 0.005 # speed of decay of temperature (the lower the slower)
+    limit = 100 # number of iterations after which no non_improving moves are accepted
+
     # run tabu search
     get_population = GetPopulation(
         nb_nurses,
@@ -40,6 +51,15 @@ if __name__ == '__main__':
         nb_nrs_per_shift,
     )
 
+    get_neighbour = GetNeighbour(
+        nb_nurses,
+        nb_work_days_per_week,
+        nb_shifts_per_work_day,
+        nrs_max_work_days_per_week,
+        covering_cost,
+    )
+
+
 
     tabu_search = TabuSearch(
         nb_nurses,
@@ -48,8 +68,10 @@ if __name__ == '__main__':
         nb_nrs_per_shift,
         nrs_max_work_days_per_week,
         nb_iter,
+        nb_neighbours,
         tabu_limit,
         get_population,
+        get_neighbour,
         covering_cost,
     )
 
@@ -68,8 +90,25 @@ if __name__ == '__main__':
         covering_cost,
     )
 
+    simulated_annealing = SimulatedAnnealing(
+        nb_nurses,
+        nb_work_days_per_week,
+        nb_shifts_per_work_day,
+        nb_nrs_per_shift,
+        nrs_max_work_days_per_week,
+        nb_iter,
+        nb_neighbours,
+        k, 
+        lam, 
+        limit,
+        get_population,
+        get_neighbour,
+        covering_cost,
+    )
+
     # solution, solution_cost, states = tabu_search.search_solution()
-    solution, solution_cost, states = genetic_algorithm.search_solution()
+    # solution, solution_cost, states = genetic_algorithm.search_solution()
+    solution, solution_cost, states = simulated_annealing.search_solution()
 
     # validate solution
     validation = Validation(
--- tabu_search/tabu_search.py
+++ tabu_search/tabu_search.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 from utils.covering_cost import CoveringCost
+from utils.get_neighbour import GetNeighbour
 from utils.get_population import GetPopulation
 
 
@@ -13,8 +14,10 @@ class TabuSearch:
         nb_nrs_per_shift: int,
         nrs_max_work_days_per_week: int,
         nb_iter: int,
+        nb_neighbours: int,
         tabu_limit: int,
         get_population: GetPopulation,
+        get_neighbour: GetNeighbour,
         covering_cost: CoveringCost,
     ) -> None:
         self.nb_nurses = nb_nurses
@@ -23,85 +26,11 @@ class TabuSearch:
         self.nb_nrs_per_shift = nb_nrs_per_shift
         self.nrs_max_work_days_per_week = nrs_max_work_days_per_week
         self.nb_iter = nb_iter
+        self.nb_neighbours = nb_neighbours
         self.tabu_limit = tabu_limit
         self.get_population = get_population
+        self.get_neighbour = get_neighbour
         self.covering_cost = covering_cost
-
-    # def get_random_nurse_schedule(self) -> np.ndarray:
-    #     nurses_worked_days_per_week = np.random.randint(
-    #         0,
-    #         self.nrs_max_work_days_per_week + 1,
-    #         1,
-    #     )
-    #     nurse_schedule = np.concatenate([
-    #         np.ones(nurses_worked_days_per_week, dtype=int),
-    #         np.zeros(
-    #             self.nb_work_days_per_week * self.nb_shifts_per_work_day -
-    #             nurses_worked_days_per_week,
-    #             dtype=int
-    #         ),
-    #     ])
-    #     nurse_schedule = np.random.permutation(nurse_schedule)
-    #     return nurse_schedule
-
-    # def get_random_initial_solution(self) -> np.ndarray:
-    #     """
-    #     Given the number of nurses, the number of shifts, and the maximum number of 
-    #     work days per week, returns a random initial solution.
-    #     Output is a numpy array of shape (nb_nurses, nb_shifts) where each element 
-    #     is either 0 or 1.
-    #     """
-    #     out = np.empty(
-    #         (
-    #             self.nb_nurses,
-    #             self.nb_work_days_per_week * self.nb_shifts_per_work_day
-    #         ),
-    #         dtype=int
-    #     )
-
-    #     for nrs in range(self.nb_nurses):
-    #         nurse_schedule = self.get_random_nurse_schedule()
-    #         out[nrs] = nurse_schedule
-    #     return out
-
-    # def covering_cost(self, solution: np.ndarray) -> int:
-    #     """
-    #     Given a solution, returns the covering cost.
-    #     """
-    #     required_coverage_per_shift = np.full(
-    #         (self.nb_work_days_per_week * self.nb_shifts_per_work_day),
-    #         self.nb_nrs_per_shift,
-    #         dtype=int,
-    #     )
-    #     coverage_per_shift = solution.sum(axis=0)
-    #     # coverage_cost = np.sum(np.maximum(0, required_coverage_per_shift - coverage_per_shift))
-    #     coverage_cost = np.sum(
-    #         np.square(required_coverage_per_shift - coverage_per_shift), axis=0)
-    #     return coverage_cost
-
-    def get_best_neighbour(
-        self, 
-        solution: np.ndarray, 
-        tabu_history: dict,
-    ) -> tuple[np.ndarray, int, dict]:
-        # generate a list of all neighbours
-        # a neighbour is a solution where one nurse has changed one shift
-        best_neighbour = np.empty(solution.shape, dtype=int)
-        best_neighbour_cost = np.inf
-
-        for nrs in range(solution.shape[0]):
-            for shift in range(solution.shape[1]):
-                neighbour = solution.copy()
-                neighbour[nrs, shift] = 1 - neighbour[nrs, shift]
-                if (tuple(map(tuple, neighbour)) in tabu_history) or \
-                    (neighbour[nrs].sum() > self.nrs_max_work_days_per_week):
-                    continue
-                neighbour_cost = self.covering_cost.covering_cost(neighbour)
-                if neighbour_cost < best_neighbour_cost:
-                    best_neighbour = neighbour
-                    best_neighbour_cost = neighbour_cost
-                    tabu_history[tuple(map(tuple, best_neighbour))] = self.tabu_limit
-        return best_neighbour, best_neighbour_cost, tabu_history
 
     def tabu_search(
         self,
@@ -120,10 +49,13 @@ class TabuSearch:
             tabu_history = {
                 sol: tabu_history[sol] for sol in tabu_history if tabu_history[sol] > 0
             }
-            best_neighbour, best_neighbour_cost, tabu_history = self.get_best_neighbour(
-                best_solution,
-                tabu_history,
-            )
+            best_neighbour, best_neighbour_cost, tabu_history = \
+                self.get_neighbour.get_neighbour_tabu(
+                    best_solution,
+                    self.nb_neighbours,
+                    tabu_history,
+                    self.tabu_limit,
+                )
             if best_neighbour_cost <= best_solution_cost:
                 best_solution = best_neighbour
                 best_solution_cost = best_neighbour_cost
